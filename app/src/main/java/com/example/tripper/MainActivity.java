332a918921c5fa0cacfcb4ee202a22d560dd1567
Jwhyee <82663161+Jwhyee@users.noreply.github.com>
2021-11-21 12:57:12 +0900

    캘린더뷰 버튼 활성화 및 액션바 삭제

--- app/src/main/java/com/example/tripper/MainActivity.java
+++ app/src/main/java/com/example/tripper/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.tripper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView calendarImg = (ImageView)findViewById(R.id.calendarImg);
+        calendarImg.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), calendarActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
